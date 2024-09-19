c5f42718d7a31cd61d220364280fbaf958a0cd82
viv1nishad <vickynishad839@gmail.com>
2024-09-19 12:31:23 +0530

    updation

---
+++ index.js
@@ -0,0 +1,18 @@
+const express =require('express');
+let connection =require('./connection');
+const student =require('./routes/student');
+const app = express();
+
+connection();
+
+app.use(student);
+
+app.listen(3000,(err)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log("server is running on 3000")
+    }
+})
+
+// locathost:3000/add/student
